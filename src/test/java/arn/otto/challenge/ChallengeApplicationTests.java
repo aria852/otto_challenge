1d541267888492b0ba28ef4794ea57ca46e85867
Aria <aria.rafinazari@gmail.com>
2022-08-31 09:54:08 +0200

    Init Otto Challenge

---
+++ src/test/java/arn/otto/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package arn.otto.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
